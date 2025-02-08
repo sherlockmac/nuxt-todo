f735f3aebfcb58d8e192fa06e39bfcf20f72ac15
Ali Afghah <ali.afghah@gmail.com>
2025-02-08 11:11:00 +0330

    Enable todo list display on About page

--- pages/about.vue
+++ pages/about.vue
@@ -1,18 +1,17 @@
 <script setup>
-// import { useTodoStore } from '@/stores/todo'
-// const store = useTodoStore()
+const store = useTodoStore()
 </script>
 
 <template>
     <div class="flex flex-col items-center gap-5">
         <h1 class="text-3xl font-bold">This is an about page</h1>
         <h2 class="text-2xl mt-5">Here's a list of completed todos:</h2>
-        <!-- <ul v-if="store">
+        <ul v-if="store">
             <li v-for="todo in store.todos" :key="todo.id">
                 <span v-if="todo.completed">
                     {{ todo.text }}
                 </span>
             </li>
-        </ul> -->
+        </ul>
     </div>
 </template>
